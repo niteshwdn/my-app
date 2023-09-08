b66039c9879f001081dfd43908b99419e9472872
Nitesh Chaudhary <niteshchaudharywdn@gmail.com>
2023-09-08 09:13:05 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Abm()
+{
+        return <p>
+        <h1>This is heading</h1>
+        <h2>This is react js</h2>
+        <h3>Welcome to react js</h3>
+        </p>;
+}
+export default Abm;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Abm from './About';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Abm></Abm>
+  </React.StrictMode>
+);
