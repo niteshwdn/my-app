59e071bd824afeec334fd0736c9f9f09b380a9de
Nitesh Chaudhary <niteshchaudharywdn@gmail.com>
2023-09-14 13:40:01 +0530

    fisrt commit

--- src/About.js
+++ src/About.js
@@ -2,13 +2,11 @@ import React, { Fragment } from "react";
 
 function Contactus()
 { 
-        const a=50;
         const Name ='Nitesh Chaudhary';
         const b=20;
         const c="";
         return(
         <Fragment>
-                <p className="hand">This is paragraph</p>
                 {Name}
                 {b+c}
                 {c ? "ok" : "no"}
