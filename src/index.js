07b8d5bc1780b45124e57d4ce35cb35a947f709f
Nitesh Chaudhary <niteshchaudharywdn@gmail.com>
2023-09-20 10:07:05 +0530

    new page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Contactus from './About';
-import Myclasscop from './Myfroms';
-import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Asd from './Mybs';
-import GridComplexExample from'./Myreactbs';
-import ButtonBaseDemo from './Uimatrial';
+import "./modules/css/style.css";
+import Mynavbar from "./modules/shares/Mynavbar";
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Mylandingpage from './modules/components/mylandingpage';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Asd/>
-    <ButtonBaseDemo/>
-    <GridComplexExample/>
-    <Myclasscop/>
-    <Contactus/>
+    <BrowserRouter>
+    <Mynavbar/>
+    <Routes>
+        <Route path='/'element={<Mylandingpage/>}></Route>
+        </Routes> 
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/modules/components/mylandingpage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Mylandingpage(){
+        return(
+                <div>Mylandingpage</div>
+        )
+}
+export default Mylandingpage
